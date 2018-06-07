becb6bb0d77f5fbd6aa810528a236eb6399b1797
Artyom Kazak <yom@artyom.me>
2018-06-07 17:33:46 +0200

    Make stuff work

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # mq-experiment
+
+## Instructions
+
+In one terminal, do:
+
+    $ docker build -t rabbitmq-mqtt .
+    $ docker run -p 5672:5672 -p 1883:1883 -p 8080:15672 -d --hostname my-rabbit --name some-rabbit -e RABBITMQ_ERLANG_COOKIE=secret rabbitmq-mqtt
+    $ docker run -it --rm --link some-rabbit:my-rabbit -e RABBITMQ_ERLANG_COOKIE=secret -e RABBITMQ_NODENAME=rabbit@my-rabbit rabbitmq-mqtt bash
+    # rabbitmqctl add_user test test && rabbitmqctl set_permissions -p / test ".*" ".*" ".*"
+
+In another terminal, do:
+
+    $ stack build --fast && stack exec -- mq-experiment --port 1883
+
+If all goes well, you should see `mq-experiment` sending and receiving messages:
+
+```
+Client server-demo sending CONNECT
+Client server-demo sending PUBLISH (d0, q1, r1, m1, 'sometopic', ... (7 bytes))
+Client server-demo received CONNACK (0)
+0
+Client server-demo sending SUBSCRIBE (Mid: 2, Topic: #, QoS: 0)
+Client server-demo received SUBACK
+(2,[0])
+Client server-demo received PUBACK (Mid: 1)
+Client server-demo sending PUBLISH (d0, q1, r1, m3, 'sometopic', ... (7 bytes))
+Client server-demo received PUBACK (Mid: 3)
+Client server-demo received PUBLISH (d0, q0, r0, m0, 'sometopic', ... (7 bytes))
+Message {mid = 0, topic = "sometopic", payload = "krendel", qos = 0, retain = False}
+```
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,65 @@
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
+
 module Main where
 
+import           Control.Concurrent
+import           Control.Monad
+import qualified Network.Mosquitto as M
+import           Network.Mosquitto.Internal.Types
+
+import           Control.Applicative
+import           Options
+
+data MainOptions = MainOptions
+    { caCert    :: String
+    , userCert  :: String
+    , userKey   :: String
+    , server    :: String
+    , serverName :: String
+    , port      :: Int
+    , keepAlive :: Int
+    }
+
+instance Options MainOptions where
+    defineOptions = pure MainOptions
+        <*> simpleOption "ca" "rootCA.pem"
+            "server's CA"
+        <*> simpleOption "cert" "cert.pem"
+            "client cert"
+        <*> simpleOption "key" "cert.key"
+            "client key"
+        <*> simpleOption "server" "localhost"
+            "server"
+        <*> simpleOption "name" "server-demo"
+            "server's name"
+        <*> simpleOption "port" 8883
+            "server's port"
+        <*> simpleOption "keep-alive" 1200
+            "server's port"
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = runCommand $ \MainOptions{..} args -> M.withMosquittoLibrary $ do
+    m <- M.newMosquitto True serverName (Just ())
+    -- M.setTls m caCert userCert userKey
+    -- M.setTlsInsecure m True
+    M.setUsernamePassword m "test" "test"  -- doesn't error out if pass is wrong :/
+    
+    M.setReconnectDelay m True 2 30
+
+    M.onMessage m print
+    M.onLog m $ const putStrLn
+    M.onConnect m $ \c -> do
+        print c
+        M.subscribe m 0 "#"
+
+    M.onDisconnect m print
+    M.onSubscribe m $ curry print
+
+    M.connect m server port keepAlive
+
+    forkIO $ forever $ do
+        M.publish m True 1 "sometopic" "krendel"
+        threadDelay 5000000
+
+    M.loopForever m
+    M.destroyMosquitto m
